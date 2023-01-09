63cc9a632e02bc4b6c495881766a29566177ba12
varun <varun.v@gida.io>
2023-01-09 18:52:07 +0530

    package functionality

---
+++ example/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pagination/pagination.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedPage = 1;
+
+  setSelectedPage(int index) {
+    setState(() {
+      selectedPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Pagination"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Pagination(
+              numOfPages: 10,
+              selectedPage: selectedPage,
+              pagesVisible: 3,
+              onPageChanged: (page) {
+                setState(() {
+                  selectedPage = page;
+                });
+              },
+              nextIcon: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.blue,
+                size: 14,
+              ),
+              previousIcon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.blue,
+                size: 14,
+              ),
+              activeTextStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+              activeBtnStyle: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38),
+                  ),
+                ),
+              ),
+              inactiveBtnStyle: ButtonStyle(
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(38),
+                )),
+              ),
+              inactiveTextStyle: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pagination.dart
+++ lib/pagination.dart
@@ -1,7 +1,105 @@
 library pagination;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class Pagination extends StatefulWidget {
+  const Pagination(
+      {super.key,
+      required this.numOfPages,
+      required this.selectedPage,
+      required this.pagesVisible,
+      required this.onPageChanged,
+      required this.activeTextStyle,
+      required this.activeBtnStyle,
+      required this.inactiveTextStyle,
+      required this.inactiveBtnStyle,
+      this.previousIcon,
+      this.nextIcon});
+
+  final int numOfPages;
+  final int selectedPage;
+  final int pagesVisible;
+  final Function onPageChanged;
+  final TextStyle activeTextStyle;
+  final ButtonStyle activeBtnStyle;
+  final TextStyle inactiveTextStyle;
+  final ButtonStyle inactiveBtnStyle;
+  final Icon? previousIcon;
+  final Icon? nextIcon;
+
+  @override
+  State<Pagination> createState() => _PaginationState();
+}
+
+class _PaginationState extends State<Pagination> {
+  late int _startPage;
+  late int _endPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _calculateVisiblePages();
+  }
+
+  @override
+  void didUpdateWidget(Pagination oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _calculateVisiblePages();
+  }
+
+  void _calculateVisiblePages() {
+    if (widget.numOfPages <= widget.pagesVisible) {
+      _startPage = 1;
+      _endPage = widget.numOfPages;
+    } else {
+      int middle = (widget.pagesVisible - 1) ~/ 2;
+      if (widget.selectedPage <= middle + 1) {
+        _startPage = 1;
+        _endPage = widget.pagesVisible;
+      } else if (widget.selectedPage >= widget.numOfPages - middle) {
+        _startPage = widget.numOfPages - (widget.pagesVisible - 1);
+        _endPage = widget.numOfPages;
+      } else {
+        _startPage = widget.selectedPage - middle;
+        _endPage = widget.selectedPage + middle;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: widget.previousIcon ?? const Icon(Icons.arrow_back_ios),
+          onPressed: widget.selectedPage > 1
+              ? () => widget.onPageChanged(widget.selectedPage - 1)
+              : null,
+        ),
+        for (int i = _startPage; i <= _endPage; i++)
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            child: TextButton(
+              style: i == widget.selectedPage
+                  ? widget.activeBtnStyle
+                  : widget.inactiveBtnStyle,
+              onPressed: () => widget.onPageChanged(i),
+              child: Text(
+                '$i',
+                style: i == widget.selectedPage
+                    ? widget.activeTextStyle
+                    : widget.inactiveTextStyle,
+              ),
+            ),
+          ),
+        IconButton(
+          icon: widget.nextIcon ?? const Icon(Icons.arrow_forward_ios),
+          onPressed: widget.selectedPage < widget.numOfPages
+              ? () => widget.onPageChanged(widget.selectedPage + 1)
+              : null,
+        ),
+      ],
+    );
+  }
 }
